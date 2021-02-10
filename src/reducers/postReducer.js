da271c883cf1aa9f7d37efb5a11bd211c54c50b2
José Bieco <59930105+JoseBieco@users.noreply.github.com>
2021-02-10 09:23:17 -0300

    Completo

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,25 @@
+import { FETCH_POSTS, NEW_POSTS } from "../actions/types";
+
+const initialState = {
+    items: [],
+    item: {}
+}
+
+export default function postFunction(state = initialState, action) {
+    switch(action.type) {
+        case FETCH_POSTS:
+            return {
+                ...state,
+                items: action.payload
+            };
+
+        case NEW_POSTS:
+            return {
+                ...state,
+                item: action.payload
+            };
+
+        default:
+            return state;
+    }
+}
